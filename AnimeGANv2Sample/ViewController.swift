69a84468fd541afabd8ee377bf1fd785155d9e85
john-rocky <rockyshikoku@gmail.com>
2024-01-20 17:43:03 +0900

    Initial Commit

---
+++ AnimeGANv2Sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimeGANv2Sample
+//
+//  Created by 間嶋大輔 on 2024/01/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
